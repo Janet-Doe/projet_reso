a21d5814b815f087a62c6db4b1cf66db1b0cbada
Melvyn <melvyn.bauvent@etu.univ-lyon1.fr>
2025-02-27 16:06:18 +0100

    feat : Timer pour arreter les threads au bout de 10 minutes

--- EightBall/src/server/CommunicationThread.java
+++ EightBall/src/server/CommunicationThread.java
@@ -1,23 +1,25 @@
 package server;
 
-import org.w3c.dom.css.Counter;
-
 import java.io.IOException;
-import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.Socket;
-import java.sql.Time;
 
 public class CommunicationThread extends Thread {
     private final Socket clientSocket;
     private final DatagramSocket threadSocket;
+    private final Timer timer;
 
     public CommunicationThread(DatagramSocket threadSocket, InetAddress clientAdr, int clientPort) throws IOException {
         this.clientSocket = new Socket(clientAdr, clientPort);
         this.threadSocket = threadSocket;
+        this.timer = new Timer(this, 600);
     }
 
     public void run() {
+        timer.start();
+        while (timer.getTime() < 600) {
+
+        }
     }
 }
---
+++ EightBall/src/server/Timer.java
@@ -0,0 +1,45 @@
+package server;
+
+import java.net.Socket;
+
+public class Timer extends Thread {
+
+    private long timeStart;
+    private boolean stop = false;
+    private final Thread thread;
+    private final int limit;
+
+    /**
+     * Timer which interrupt the Thread given when limit is passed.
+     * @param thread Thread
+     * @param limit int, time in seconds
+     */
+    public Timer(Thread thread, int limit) {
+        this.thread = thread;
+        this.limit = limit;
+    }
+
+    public int getTime() {
+        long currentTime = System.currentTimeMillis();
+        return (int) (currentTime - timeStart)*1000;
+
+    }
+
+    public void reset() {
+        this.timeStart = System.currentTimeMillis();
+    }
+
+    public void close() {
+        this.stop = true;
+    }
+
+    @Override
+    public void run() {
+        this.timeStart = System.currentTimeMillis();
+        while (!stop) {
+            if (getTime() > limit) {
+                this.thread.interrupt();
+            }
+        }
+    }
+}
