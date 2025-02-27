74d1995e86063678af1682faa7b05bfeceec3d9c
Melvyn <melvyn.bauvent@etu.univ-lyon1.fr>
2025-02-27 12:21:26 +0100

    feat : création Main du server

--- EightBall/src/server/Main.java
+++ EightBall/src/server/Main.java
@@ -1,7 +1,31 @@
 package server;
 
+import java.net.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Je suis le serveur");
+        DatagramSocket serverSocket = null;
+        try {
+            InetSocketAddress serverAddress = new InetSocketAddress("localhost", 6666);
+            serverSocket = new DatagramSocket(serverAddress);
+            byte[] receptionBuffer = new byte[1024];
+
+            while (true) {
+                DatagramPacket firstPacket = new DatagramPacket(receptionBuffer, receptionBuffer.length);
+                serverSocket.receive(firstPacket);
+
+                InetAddress clientAdr = firstPacket.getAddress();
+                int clientPort = firstPacket.getPort();
+                DatagramSocket threadSocket = new DatagramSocket(serverAddress);
+
+                CommunicationThread newThread = new CommunicationThread(threadSocket, clientAdr, clientPort);
+                newThread.start();
+            }
+        } catch (Exception e) {
+            if (serverSocket != null) {
+                serverSocket.close();
+            }
+            System.err.println(e);
+        }
     }
 }
